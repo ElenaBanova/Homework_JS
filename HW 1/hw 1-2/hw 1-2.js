93a553a07db37bb7cf355b27302389d0ea7024ed
ElenaBanova <banovaolena52@gmail.com>
2024-11-05 01:02:53 +0200

    homework JS for lesson 1

---
+++ HW 1/hw 1-2/hw 1-2.js
@@ -0,0 +1,5 @@
+let firstName = 'Oлена';
+let middleName = 'Андріївна';
+let lastName = 'Банова';
+let person = `${firstName} ${middleName} ${lastName}`;
+console.log(person);
---
+++ HW 1/hw 1-4/hw 1-4.js
@@ -0,0 +1,12 @@
+let name = prompt('Ім\'я', 'Олена');
+console.log(name);
+
+let middleName = prompt('По-батькові', 'Андріївна');
+console.log(middleName);
+
+let age =prompt('Вік', '31');
+console.log(age);
+
+
+
+
