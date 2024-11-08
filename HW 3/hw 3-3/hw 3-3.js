a0159ace6d50b0aa274a9989565f79f9e2623ef1
ElenaBanova <banovaolena52@gmail.com>
2024-11-08 18:45:27 +0200

    homework js lesson 3

---
+++ HW 3/hw 3-3/hw 3-3.js
@@ -0,0 +1,4 @@
+let x=0;
+while (x<20){
+    document.write(`<h1>Lorem ipsum dolor sit amet.</h1>`); x++
+}
---
+++ HW 3/hw 3-4/hw 3-4.js
@@ -0,0 +1,5 @@
+let x = 0;
+while (x < 20) {
+    document.write(`<h1>Lorem ipsum dolor sit. ${x}</h1>`);
+    x++
+}
