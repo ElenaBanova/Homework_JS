86de16a161861c6deb9104998a5782c7acd1e4ab
ElenaBanova <banovaolena52@gmail.com>
2024-11-21 20:15:42 +0200

    homework js lesson 8

---
+++ HW 8/hw 8-1/hw 8-1.js
@@ -0,0 +1,21 @@
+function copy(course) {
+    let functionsCourse = [];
+    for (const key in course){
+        if (typeof course[key] === 'function'){
+            const functionClone = course[key].bind({});
+            functionsCourse.push({functionClone, key});
+        }
+    }
+    if (typeof course === 'object') {
+        const courseClone = JSON.parse((JSON.stringify(course)));
+        for (const func of functionsCourse){
+            courseClone[func.key] = func.functionClone;
+        }
+        return courseClone;
+    }
+    throw new Error('Error 222');
+}
+let value1 = copy({title: 'JavaScript Complex', monthDuration: 5, geeting(){console.log('Hello people')}, foo(a){console.log(a)}});
+console.log(value1)
+value1.geeting();
+value1.foo(325);
