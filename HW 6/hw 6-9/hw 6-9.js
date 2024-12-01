bb971344f6046e9ec812c4ad9e72de3e5e71466b
ElenaBanova <banovaolena52@gmail.com>
2024-12-01 15:16:03 +0200

    homework js lesson 6

--- HW 6/hw 6-9/hw 6-9.js
+++ HW 6/hw 6-9/hw 6-9.js
@@ -40,7 +40,8 @@ deck.push(deckOfCards('ace', 'diamonds', 'red'));
 deck.push(deckOfCards('ace', 'clubs', 'black'));
 console.log(deck)
 
-console.log(deck.filter(value => value.suit === 'spades' && value.name === 'ace'));
+console.log(deck.find(value=>value.name === 'ace' && value.suit === 'spade'));
+// console.log(deck.filter(value => value.suit === 'spades' && value.name === 'ace'));
 console.log(deck.filter(value => value.name === 6));
 console.log(deck.filter(value => value.color === 'red'));
 console.log(deck.filter(value => value.suit === 'diamonds'));
@@ -48,9 +49,34 @@ console.log(deck.filter(value => {
     if (typeof value.name === "string" && value.suit === 'clubs'){
         return value
     }
-    if (value.name >8 && value.suit === 'clubs'){
+    if (value.name >9 && value.suit === 'clubs'){
         return value
     }
 }))
 
-
+// const suits = ['spade', 'diamond', 'heart', 'club'];
+// const values = ['6', '7', '8', '9', '10', 'queen', 'king', 'ace'];
+//
+// const cards=[];//масив колоды карт
+// for (const suit of suits){//итеруем асив suit
+//     for (const value of values){//итеруем масив value
+//         const card = {cardSuit: suit, value: value};//формируем каждую карту колоды
+//         if (suit === 'heart' || suit === 'diamod'){//если карта с мастью сердце или мастью бубен
+//             card.color = 'red'; //присваиваем новое свойство этим картам красный цвет
+//         }else {
+//             card.color = 'black';//остальные карты будут иметь свойство черные
+//         }
+//         cards.push(card);//передаем все в масив cards
+//     }
+//
+// }
+// console.log(cards);//выводим колоду в консоль
+//
+// //через фильтр делать нельзя было потому что фильтр формирует масив, а нам нужно вывести одну карту
+// console.log(cards.find(card=>card.value === 'ace' && card.cardSuit === 'spade'));
+// //получить каждую карту отдельно если его характеристика соответствует указаным параметрам
+// console.log(cards.filter(card => card.value === '6'));
+// console.log(cards.filter(card => card.color === 'red'));
+// console.log(cards.filter(card => card.cardSuit === 'diamond'));
+// console.log(cards.filter(card => card.cardSuit === 'club' && card.value !=='6' || card.value !== '7' || card.value !== '8' || card.value !== '9'));
+//
